80bc31185bfdfbb0f1512f20cb3c0abde7962d57
Josh Blum <josh@joshknows.com>
2016-06-27 16:56:30 -0700

    added rate and automatic event to signal probe

--- utility/SignalProbe.cpp
+++ utility/SignalProbe.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2014-2015 Josh Blum
+// Copyright (c) 2014-2016 Josh Blum
 // SPDX-License-Identifier: BSL-1.0
 
 #include <Pothos/Framework.hpp>
@@ -6,6 +6,7 @@
 #include <complex>
 #include <iostream>
 #include <algorithm> //min/max
+#include <chrono>
 
 /***********************************************************************
  * |PothosDoc Signal Probe
@@ -13,6 +14,8 @@
  * The signal probe block records the last calculation from a stream of elements.
  * The signal probe has a slot called "probeValue" will will cause
  * a signal named "valueTriggered" to emit the most recent value.
+ * The probe will also emit the value automatically at the specified rate
+ * using the "valueChanged" signal.
  *
  * The calculation for value can be, the last seen value,
  * the RMS (root mean square) over the last buffer,
@@ -37,11 +40,19 @@
  * |option [RMS] "RMS"
  * |option [Mean] "MEAN"
  *
+ * |param rate How many calculations per second?
+ * The probe will perform a calculation at most this many times per second.
+ * Incoming samples will be dropped and not processed between calculations.
+ * A special value of 0.0 means perform the calculation on every input window.
+ * |preview valid
+ * |default 0.0
+ *
  * |param window How many elements to calculate over?
  * |default 1024
  *
  * |factory /comms/signal_probe(dtype)
  * |setter setMode(mode)
+ * |setter setRate(rate)
  * |setter setWindow(window)
  **********************************************************************/
 template <typename Type, typename ProbeType>
@@ -51,7 +62,8 @@ public:
     SignalProbe(void):
         _value(0),
         _mode("VALUE"),
-        _window(1024)
+        _window(1024),
+        _rate(0.0)
     {
         this->setupInput(0, typeid(Type));
         this->registerCall(this, POTHOS_FCN_TUPLE(SignalProbe, value));
@@ -59,7 +71,10 @@ public:
         this->registerCall(this, POTHOS_FCN_TUPLE(SignalProbe, getMode));
         this->registerCall(this, POTHOS_FCN_TUPLE(SignalProbe, setWindow));
         this->registerCall(this, POTHOS_FCN_TUPLE(SignalProbe, getWindow));
+        this->registerCall(this, POTHOS_FCN_TUPLE(SignalProbe, setRate));
+        this->registerCall(this, POTHOS_FCN_TUPLE(SignalProbe, getRate));
         this->registerProbe("value");
+        this->registerSignal("valueChanged");
         this->input(0)->setReserve(1);
     }
 
@@ -89,12 +104,38 @@ public:
         return _window;
     }
 
+    void setRate(const double rate)
+    {
+        _rate = rate;
+    }
+
+    double getRate(void) const
+    {
+        return _rate;
+    }
+
+    void activate(void)
+    {
+        _nextCalc = std::chrono::high_resolution_clock::now();
+    }
+
     void work(void)
     {
         auto inPort = this->input(0);
         auto x = inPort->buffer().template as<const Type *>();
         const auto N = std::min(_window, inPort->elements());
         inPort->consume(N);
+
+        //check if the time expired or rate is 0.0
+        auto currentTime = std::chrono::high_resolution_clock::now();
+        if (_rate != 0.0 and currentTime < _nextCalc) return;
+
+        //increment for the next calculation time
+        if (_rate != 0.0)
+        {
+            const auto tps = std::chrono::nanoseconds((long long)(1e9/_rate));
+            _nextCalc += std::chrono::duration_cast<std::chrono::high_resolution_clock::duration>(tps);
+        }
 
         if (_mode == "VALUE") _value = x[N-1];
         else if (_mode == "RMS")
@@ -116,12 +157,16 @@ public:
             mean /= N;
             _value = mean;
         }
+
+        this->callVoid("valueChanged", _value);
     }
 
 private:
     ProbeType _value;
     std::string _mode;
     size_t _window;
+    double _rate;
+    std::chrono::high_resolution_clock::time_point _nextCalc;
 };
 
 /***********************************************************************
